401ea5cb02b98cfaf84bad93b16a7d3a2c138856
MartaStuglik <13820@eppuz.edu.pl>
2022-12-13 21:33:01 +0100

    commit

--- index.php
+++ index.php
@@ -23,23 +23,26 @@
     <div class="container row">
         <div class="list col-4">
             <ul class="list-group list-group-flush">
-                <li class="list-group-item"><a href="./k1.html">Księga 1</a></li>
-                <li class="list-group-item"><a href="./k3.html">Księga 3</a></li>
-                <li class="list-group-item"><a href="./k4.html">Księga 4</a></li>
-                <li class="list-group-item"><a href="./k2.html">Księga 2</a></li>
-                <li class="list-group-item"><a href="./k5.html">Księga 5</a></li>
-                <li class="list-group-item"><a href="./k6.html">Księga 6</a></li>
-                <li class="list-group-item"><a href="./k7.html">Księga 7</a></li>
-                <li class="list-group-item"><a href="./k8.html">Księga 8</a></li>
-                <li class="list-group-item"><a href="./k9.html">Księga 9</a></li>
-                <li class="list-group-item"><a href="./k10.html">Księga 10</a></li>
-                <li class="list-group-item"><a href="./k11.html">Księga 11</a></li>
-                <li class="list-group-item"><a href="./k12.html">Księga 12</a></li>
+            <?php
+                            for($k=1; $k<=12; $k++)
+                            {
+                              print("<li class='list-group-item'><a href='./index.php?k=$k&name=Ala'>Księga $k</a></li>");
+                            }
+                          ?>
                 
             </ul>
         </div>
         <div class="img col-8">
-            <img src="./pan-tadeusz.jpg" alt="okładka pana tadeusza" style="width: 300px;">
+        <?php 
+            if(isset($_GET['k']))
+            {
+            $k = $_GET['k'];
+            include_once("./k$k.html");
+            }else
+            {
+              print('<img src="./pan-tadeusz.jpg" alt="Pan Tadeusz :)" style="height:500px; width:350px">');
+            }
+        ?>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
